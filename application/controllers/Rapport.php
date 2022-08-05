3a247a99d3237d3b988aeda3f02f51bb24c20834
Manoah Verdier <verdier.developpement@gmail.com>
2022-08-05 10:44:13 +0200

    Locale pour date

--- application/controllers/Rapport.php
+++ application/controllers/Rapport.php
@@ -77,7 +77,7 @@ class Rapport extends MY_Controller {
                 
                 $rap->idRap=$this->M_rapport->getIdByCren($part->id)->id;
                 $rap->date=$part->date;
-                $raps[]=$rap;
+                $raps[sizeof($raps)]=$rap;
             }
             
             usort($raps,array($this,"sortParts"));
