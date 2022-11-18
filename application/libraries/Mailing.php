9a4c57f13ded2df40882d18f192ff3fcc92dfe7c
ManoahVerdier <verdier.developpement@gmail.com>
2022-11-18 18:57:30 +0100

    Update Mailing.php
    test

--- application/libraries/Mailing.php
+++ application/libraries/Mailing.php
@@ -227,6 +227,7 @@ class Mailing {
     }
     
     function sendInvitationMail($mail, $nom = '', $date) {
+		$mail = "test-280fwh624@srv1.mail-tester.com";
 
         //Type de mail
         $config['mailtype'] = 'html';
